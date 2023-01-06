30eaa9479fb262a3d5d00ec6f4d0e20aebb8eba9
Shruti Khule <86286310+shruti-khule@users.noreply.github.com>
2023-01-06 23:23:06 +0100

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 3000
+    })
+  }); 
+
+ window.onscroll = function() {
+ 	myScroll()
+ };
+ var navbar = document.getElementById("navbar");
+ var sticky = navbar.offsetTop;
+ 
+ function myScroll() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } 
+  else {
+    navbar.classList.remove("sticky");
+  }
+}
